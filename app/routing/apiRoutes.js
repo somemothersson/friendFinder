8a59d01b97651e3e982331b49d2eebfb491e3803
Stephen Malito <stevemalito@gmail.com>
2019-06-17 14:26:34 -0500

    setting up route files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,25 @@
+
+  
+  // Displays all friends
+  app.get("/api/friend", function(req, res) {
+    return res.json(friends);
+  });
+  
+ 
+  // Create New Characters - takes in JSON input
+  app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newcharacter = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newcharacter.routeName = characters.length ++;
+  
+    console.log(newcharacter);
+  
+    characters.push(newcharacter);
+  
+    res.json(newcharacter);
+  });
+  
---
+++ app/server.js
@@ -0,0 +1,28 @@
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+require("./app/routes/html-routes.js")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
