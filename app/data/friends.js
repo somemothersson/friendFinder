ef3e90134538828b51bb0243bf12a34f0762e71c
Stephen Malito <stevemalito@gmail.com>
2019-06-17 18:25:15 -0500

    html started

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,20 @@
-var characters = [
+let users = [
     {
-      routeName: "yoda",
-      name: "Yoda",
-      role: "Jedi Master",
-      age: 900,
-      forcePoints: 2000
-    },
-    {
-      routeName: "darthmaul",
-      name: "Darth Maul",
-      role: "Sith Lord",
-      age: 200,
-      forcePoints: 1200
-    },
-    {
-      routeName: "obiwankenobi",
-      name: "Obi Wan Kenobi",
-      role: "Jedi Master",
-      age: 55,
-      forcePoints: 1350
-    }
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+    
   ];
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,10 @@
 
-  
+  // var friend = require("../data/friends.js");
   // Displays all friends
-  app.get("/api/friend", function(req, res) {
-    return res.json(friends);
+
+  module.exports = function (app) {
+  app.get("/api/friends", function(req, res) {
+    return res.json(users);
   });
   
  
@@ -10,16 +12,14 @@
   app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newcharacter = req.body;
+    var newUser = req.body;
   
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newcharacter.routeName = characters.length ++;
+    
   
-    console.log(newcharacter);
+    console.log(newUser);
   
-    characters.push(newcharacter);
+    users.push(newUser);
   
-    res.json(newcharacter);
+    res.json(newUser);
   });
-  
+}
